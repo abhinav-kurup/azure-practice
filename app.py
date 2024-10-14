441193c6b230ec979c7c6ac56d353eb26b5f03f0
abhinav-kurup <abhinavkurup2002@gmail.com.com>
2024-10-14 16:32:17 +0530

    code changed

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Flask Deployment sucessful</h1></body></html>\n"
+    return "<html><body><h1>Flask Deployment sucessful chenged</h1></body></html>\n"
