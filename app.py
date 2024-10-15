d3c9ba5006b1f25d81e028e1751b4daae3ea6e17
abhinav-kurup <abhinavkurup2002@gmail.com.com>
2024-10-15 10:54:05 +0530

    added env variables

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Flask Deployment successful changed</h1></body></html>\n"
+    key = os.environ.get('key')
+    return f"<html><body><h1>Flask Deployment successful, Key: {key}</h1></body></html>\n"
